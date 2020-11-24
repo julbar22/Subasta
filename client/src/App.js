68271aaf10b96af615faba678a5f61ce1e4606ea
Julian Barbosa <julian.barbosa@3dves.com>
2020-11-23 23:59:15 -0300

    Version Inicial de un smartContract

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import ReadString from "./ReadString";
+import SetString from "./SetString";
+
+class App extends React.Component {
+  state = { loading: true, drizzleState: null };
+
+  componentDidMount() {
+    const { drizzle } = this.props;
+
+    console.log(drizzle)
+    // subscribe to changes in the store
+    this.unsubscribe = drizzle.store.subscribe(() => {
+
+      // every time the store updates, grab the state from drizzle
+      const drizzleState = drizzle.store.getState();
+      // check to see if it's ready, if so, update local component state
+      if (drizzleState.drizzleStatus.initialized) {
+        //const chainId = drizzle.web3.eth;
+        this.setState({ loading: false, drizzleState });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    if (this.state.loading) return "Loading Drizzle...";
+    return (
+      <div className="App">
+        <ReadString
+          drizzle={this.props.drizzle}
+          drizzleState={this.state.drizzleState}
+        />
+        <SetString
+          drizzle={this.props.drizzle}
+          drizzleState={this.state.drizzleState}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
